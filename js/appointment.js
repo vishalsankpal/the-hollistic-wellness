7767584a764017d3394b7c9d1887b6ffc990f5e9
spol9967 <spol421@gmail.com>
2019-12-26 01:54:26 +0530

    appointment js

---
+++ js/appointment.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#datetimepicker3').datetimepicker({
+        format: 'LT'
+    });
+    // $("#appointmentbtn").click(function () {
+    //     $("#appointmentbtn").hide();
+        
+    //     $.get("http://localhost:5001/the-holistic-wellness/us-central1/sendMail",
+    //     {
+    //         name: "Donald Duck",
+    //         emailid: "abc@abc.com",
+    //         service: "first class",
+    //         time: "1.30",
+    //         note: "HAHAHA"
+    //     },
+    //     function (data, status) {
+    //         alert("Data: " + data + "\nStatus: " + status);
+    //         $("#appointmentbtn").show();
+    //     });
+
+    // });
+    
+});
