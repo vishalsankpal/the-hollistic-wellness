4e6d7de45d92e52e7a669da459e2b08ceb222976
spol9967 <spol421@gmail.com>
2019-12-28 23:35:37 +0530

    Conflict merge

--- js/appointment.js
+++ js/appointment.js
@@ -1,23 +1,13 @@
 $(document).ready(function () {
-    $('#datetimepicker3').datetimepicker({
-        format: 'LT'
-    });
-    // $("#appointmentbtn").click(function () {
-    //     $("#appointmentbtn").hide();
-        
-    //     $.get("http://localhost:5001/the-holistic-wellness/us-central1/sendMail",
-    //     {
-    //         name: "Donald Duck",
-    //         emailid: "abc@abc.com",
-    //         service: "first class",
-    //         time: "1.30",
-    //         note: "HAHAHA"
-    //     },
-    //     function (data, status) {
-    //         alert("Data: " + data + "\nStatus: " + status);
-    //         $("#appointmentbtn").show();
-    //     });
-
-    // });
     
+    $( ".make-an-appointment" ).click(function() {
+        $('#appointmentform').submit();
+        $(".make-an-appointment").hide();
+        var params = $( this ).serialize();
+        $.get("http://localhost:5001/the-holistic-wellness/us-central1/sendMail"+params,
+        function (data, status) {
+            alert("Appointment booked"+params);
+            $(".make-an-appointment").show();
+        });
+      });
 });
