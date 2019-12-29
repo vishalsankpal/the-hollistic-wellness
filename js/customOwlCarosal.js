e12d4965c43662be10d8ad3c77ba2859fd95996a
spol9967 <spol421@gmail.com>
2019-12-29 19:24:08 +0530

    changes

--- js/customOwlCarosal.js
+++ js/customOwlCarosal.js
@@ -1,16 +1,16 @@
 $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 15,
-    nav: false,
-    responsive: {
-        0: {
-            items: 1
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
         },
-        600: {
-            items: 1
+        600:{
+            items:3
         },
-        1000: {
-            items: 2
+        1000:{
+            items:3
         }
     }
 })
