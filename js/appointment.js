b1f7996dc911e1ccf14cfd810d8a01bcbc35413b
dipti sindkar <diprsindkar11@gmail.com>
2019-12-30 02:15:42 +0530

    ajax

--- js/appointment.js
+++ js/appointment.js
@@ -1,23 +1,8 @@
-$(document).ready(function () {
-    
-    $( ".make-an-appointment" ).click(function(event) {
-        event.preventDefault();
-        console.log($('#appointmentform').serialize());
-        // $.ajax({
-        //     url:'',
-        //     type:'get',
-        //     data:$('#appointment-form').serialize(),
-        //     success:function(result){
-        //         console.log(result);
-        //     }
-        // })
-        // $('#appointmentform').submit();
-        // var params = $('#appointment-form').serialize();
-        // console.log(params);
-        // $.get("https://us-central1-the-holistic-wellness.cloudfunctions.net/sendMail"+params,
-        // function (data, status) {
-        //     alert("Appointment booked"+params);
-        //     $(".make-an-appointment").show();
-        // });
-      });
-});
+// 
+$(document).ready(function(){
+    $('#appointment').click(function(e){
+        e.preventDefault();
+        var x = $('#appointmentform').serialize();
+        console.log(x);
+    })
+})
